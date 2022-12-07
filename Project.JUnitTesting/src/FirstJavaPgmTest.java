0d3a0733913f6e654b6f5b0216e2b0ab1925fcd3
ThejaNagaraj <120013669+ThejaNagaraj@users.noreply.github.com>
2022-12-07 15:12:07 +0530

    Add files via upload

---
+++ Project.JUnitTesting/src/FirstJavaPgmTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class FirstJavaPgmTest {
+	@Test
+    void disp()
+    {
+      System.out.println("Hello All");	
+    }
+	@Test
+    void disp2()
+    {
+      System.out.println("How you all doing");	
+    }
+}
